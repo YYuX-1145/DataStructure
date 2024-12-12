4f4f72418b2cf247465f79c139b0618985fdb2ca
YYuX-1145 <you@example.com>
2024-12-12 19:15:30 +0800

    kmp.cpp

--- kmp.cpp
+++ kmp.cpp
@@ -4,11 +4,13 @@
 using namespace std;
 int kmp(const string str1,const string str2)
 {
+    int ret=0;
+    vector<int> pos;
     if(str1.length()<str2.length())
     {
         return 0;
     }
-    vector<int> next(str2.length()-1);
+    vector<int> next(str2.length());
     
     next[0] = 0;
     for (int i = 1, j = 0; i < str2.length(); i++)
@@ -18,16 +20,43 @@ int kmp(const string str1,const string str2)
             j = next[next[j-1]];
         }            
         if (str2[j] == str2[i])
+        {
             j++;
-        next[i] = j;
+        }            
+        next[i] = j;        
     }
 
-
+    int ptr2=0;
+    int p=0;//
+    for(auto c1:str1)
+    {
+        if(ptr2>0&&c1!=str2[ptr2])
+        {
+            ptr2=next[ptr2-1];
+        }
+        if(c1==str2[ptr2])
+        {
+            ptr2++;
+        }
+        if(ptr2==str2.length())
+        {
+            ptr2=0;
+            ret++;
+            pos.push_back(p-(str2.length()-1));//
+        }
+        p++;//         
+    }
+    //
+    for(auto x:pos)
+    cout<<x<<' ';
+    cout<<endl;
+    //
+    return ret;
 }
 
 int main()
 {    
-    string str1 = "abcbdbbbacba";
-    string str2 = "bacba";
-    string str3 = "01234";
+    string str1 = "abababbbacbababaababa";
+    string str2 = "ababa";
+    cout<<kmp(str1,str2);
 }
